4a7306e7a2757c1ef170da9fe8db25eeffb45072
bngoc156 <phbaongoc2001@gmail.com>
2023-04-26 19:51:28 +0700

    fix hover

--- public/scripts/javascript.js
+++ public/scripts/javascript.js
@@ -2,7 +2,7 @@ document
   .querySelectorAll(".nav-item a")
   .forEach((e) => e.addEventListener("click", (_) => change(e.dataset.id)));
 function change(n) {
-  let panels = document.querySelectorAll(".card-body");
-  panels.forEach((p) => p.classList.remove("active"));
-  panels[n - 1].classList.add("active");
+  let contents = document.querySelectorAll(".card-body");
+  contents.forEach((p) => p.classList.remove("active"));
+  contents[n - 1].classList.add("active");
 }
